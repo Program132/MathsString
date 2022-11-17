3fc105f49a12ca8c5e58161d6ff004d0dafdb8fe
Program <67320703+Program132@users.noreply.github.com>
2022-11-17 20:10:40 +0100

    WIP (Test on Multi + Divs)

--- main.cpp
+++ main.cpp
@@ -3,7 +3,6 @@
 #include <string>
 #include <sstream>
 #include <stdexcept>
-#include <algorithm>
 #include <tuple>
 
 int convertInt(std::string& n) {
@@ -32,6 +31,15 @@ std::vector<std::string> split(std::string& arg) {
     return myVec;
 }
 
+bool hasElementNA(std::vector<std::string> array) {
+    auto res = std::find(begin(array), end(array), "N/A");
+
+    if (res != array.end()) {
+        return true;
+    }
+    return false;
+}
+
 std::tuple<bool, int> expectMulti(std::vector<std::string> array) {
     auto res = std::find(begin(array), end(array), "*");
 
@@ -50,7 +58,7 @@ std::tuple<bool, int> expectDiv(std::vector<std::string> array) {
     return {false, 0};
 }
 
-std::tuple<bool, int> expectAdd(std::vector<std::string> array) {
+[[maybe_unused]] std::tuple<bool, int> expectAdd(std::vector<std::string> array) {
     auto res = std::find(begin(array), end(array), "+");
 
     if (res != array.end()) {
@@ -59,7 +67,7 @@ std::tuple<bool, int> expectAdd(std::vector<std::string> array) {
     return {false, 0};
 }
 
-std::tuple<bool, int> expectRemove(std::vector<std::string> array) {
+[[maybe_unused]] std::tuple<bool, int> expectRemove(std::vector<std::string> array) {
     auto res = std::find(begin(array), end(array), "-");
 
     if (res != array.end()) {
@@ -73,9 +81,41 @@ int main() {
 
     std::vector<std::string> contentSplitted = split(content);
 
-    auto [operatorMulti, caseVector] = expectMulti(contentSplitted);
+    auto [operatorMulti, caseVectorMulti] = expectMulti(contentSplitted);
 
+    if (operatorMulti) {
+        int n1 = convertInt(contentSplitted[caseVectorMulti - 1]);
+        int n2 = convertInt(contentSplitted[caseVectorMulti + 1]);
+        int result = n1 * n2;
+        contentSplitted[caseVectorMulti - 1] = std::to_string(result);
+        contentSplitted[caseVectorMulti] = "N/A";
+        contentSplitted[caseVectorMulti + 1] = "N/A";
 
+        while (hasElementNA(contentSplitted)) {
+            auto it = std::find(begin(contentSplitted), end(contentSplitted), "N/A");
+            contentSplitted.erase(it);
+        }
+    }
+
+    auto [operatorDiv, caseVectorDiv] = expectDiv(contentSplitted);
+
+    if (operatorDiv) {
+        int n1 = convertInt(contentSplitted[caseVectorDiv-1]);
+        int n2 = convertInt(contentSplitted[caseVectorDiv+1]);
+        int result = n1 / n2;
+        contentSplitted[caseVectorDiv - 1] = std::to_string(result);
+        contentSplitted[caseVectorDiv] = "N/A";
+        contentSplitted[caseVectorDiv + 1] = "N/A";
+
+        while (hasElementNA(contentSplitted)) {
+            auto it = std::find(begin(contentSplitted), end(contentSplitted), "N/A");
+            contentSplitted.erase(it);
+        }
+    }
+
+    for (auto const& e : contentSplitted) {
+        std::cout << e << std::endl;
+    }
 
     return 0;
 }

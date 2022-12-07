97b40e9cc5172dcbb5f649784e03c400c84a816c
Program <67320703+Program132@users.noreply.github.com>
2022-12-07 21:33:14 +0100

    Tests

--- main.cpp
+++ main.cpp
@@ -1,113 +1,39 @@
 #include <iostream>
 #include <vector>
 #include <string>
-#include <sstream>
-#include <stdexcept>
-#include <tuple>
 
-int convertInt(std::string& n) {
-    try {
-        int v = std::stoi(n);
-        return v;
-    }
-    catch (std::invalid_argument const& ex) {
-        std::cout << "It can not be a int value !" << " Final error: " << ex.what() << std::endl;
-    }
+#include "src/Tokenizer.h"
+#include "src/Parser.h"
 
-    return 0;
-}
-
-std::vector<std::string> split(std::string& arg) {
-    std::string myBuffer;
-
-    std::vector<std::string> myVec;
-
-    std::stringstream ss;
-    ss << arg;
-    while (ss >> myBuffer) {
-        myVec.push_back(myBuffer);
-    }
-
-    return myVec;
-}
-
-bool hasElement(std::vector<std::string> array, std::string element) {
-    auto res = std::find(begin(array), end(array), element);
-
-    if (res != array.end()) {
-        return true;
-    }
-    return false;
-}
-
-int expectElement(std::vector<std::string> array, std::string element) {
-    auto res = std::find(begin(array), end(array), element);
-
-    if (res != array.end()) {
-        return std::distance(array.begin(), res);
-    }
-    return 0;
-}
-
-void operations(std::vector<std::string>& array, int& caseVector, std::string op) {
-    int n1 = convertInt(array[caseVector-1]);
-    int n2 = convertInt(array[caseVector+1]);
-    int result = 0;
-    if (op == "-") {
-        result = n1 - n2;
-    } else if (op == "+") {
-        result = n1 + n2;
-    } else if (op == "*") {
-        result = n1 * n2;
-    } else if (op == "/") {
-        result = n1 / n2;
-    }
-    array[caseVector - 1] = std::to_string(result);
-    array[caseVector] = "N/A";
-    array[caseVector + 1] = "N/A";
-
-    while (hasElement(array, "N/A")) {
-        auto it = std::find(begin(array), end(array), "N/A");
-        array.erase(it);
-    }
-}
+using namespace MathParser;
 
 int main() {
-    std::string content = "9 + 10";
+    std::string content = "9 + 10 * 20 - 5";
+    TokenBuilding Tokenizer;
+    std::vector<Token> contentMathTokens = MathParser::TokenBuilding::ParserTokens(content);
+    double result = parse(contentMathTokens);
+    std::cout << result << std::endl;
 
-    std::vector<std::string> contentSplitted = split(content);
+    content = "9 - 9";
+    contentMathTokens = MathParser::TokenBuilding::ParserTokens(content);
+    result = parse(contentMathTokens);
+    std::cout << result << std::endl;
 
-    int totalSizeVector = contentSplitted.size();
+    content = "9 / 9";
+    contentMathTokens = MathParser::TokenBuilding::ParserTokens(content);
+    result = parse(contentMathTokens);
+    std::cout << result << std::endl;
 
-    int i = 0;
+    content = "9 * 9";
+    contentMathTokens = MathParser::TokenBuilding::ParserTokens(content);
+    result = parse(contentMathTokens);
+    std::cout << result << std::endl;
 
-    while (i < totalSizeVector) {
+    content = "9 + 9";
+    contentMathTokens = MathParser::TokenBuilding::ParserTokens(content);
+    result = parse(contentMathTokens);
+    std::cout << result << std::endl;
 
-        while (hasElement(contentSplitted, "*")) {
-            auto caseVectorMulti = expectElement(contentSplitted, "*");
-            operations(contentSplitted, caseVectorMulti, "*");
-        }
-
-
-        while (hasElement(contentSplitted, "/")) {
-            auto caseVectorDiv = expectElement(contentSplitted, "/");
-            operations(contentSplitted, caseVectorDiv, "/");
-        }
-
-        while (hasElement(contentSplitted, "+")) {
-            auto caseVectorAdd = expectElement(contentSplitted, "+");
-            operations(contentSplitted, caseVectorAdd, "+");
-        }
-
-        while (hasElement(contentSplitted, "-")) {
-            auto caseVectorRemove = expectElement(contentSplitted, "-");
-            operations(contentSplitted, caseVectorRemove, "-");
-        }
-        i++;
-    }
-
-    int final_result = convertInt(contentSplitted[0]);
-    std::cout << final_result << std::endl;
 
     return 0;
 }

223a57a3e7435038386322c5dd9b455e2c13b52f
Program <67320703+Program132@users.noreply.github.com>
2022-12-07 21:33:48 +0100

    Tokenizer class functions.

---
+++ src/Tokenizer.h
@@ -0,0 +1,41 @@
+#pragma once
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <stdexcept>
+
+namespace MathParser {
+    enum AllTokenTypes {
+        WHITESPACE,
+        INT,
+        DOUBLE,
+        MAYBE_DOUBLE,
+        OPERATOR
+    };
+
+    static const char *sAllTokenTypes[] = {
+            "WHITESPACE",
+            "INT",
+            "DOUBLE",
+            "MAYBE_DOUBLE",
+            "OPERATOR"
+    };
+
+    class Token {
+    public:
+        enum AllTokenTypes TokenType;
+        std::string TokenText;
+
+        void DebugPrint() const;
+
+        friend bool operator==(Token const& a, Token const& b);
+    };
+
+    class TokenBuilding {
+    public:
+        static std::vector<Token> ParserTokens(const std::string& content);
+
+        static void endToken(Token &token, std::vector<Token> &tokens);
+    };
+}
